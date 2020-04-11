0eff2c390e9687698de3d7b3de77c9d8d34df588
Gabriel Passos <contato.gabrielhp@gmail.com>
2020-04-11 03:27:01 -0300

    Login page started

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+import './style.css';
 
 export default class Logon extends Component {
   render() {
     return (
-      <h1>Hello Login Page</h1>
+      <>
+        <header className="">
+          <nav className="menus">
+            <div className="menu-principal">
+              <ul className="link-menu">
+                <li><Link className="" to="/">Home</Link></li>
+                <li><Link className="" to="/register">Register</Link></li>
+                <li><Link className="link-menu-login" to="/profile">Sign in</Link></li>
+              </ul>
+            </div>
+            <div className="menu-secundario">
+              <ul>
+                <li><Link className="" to="/">Test</Link></li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+      </>
     )
   }
 }
