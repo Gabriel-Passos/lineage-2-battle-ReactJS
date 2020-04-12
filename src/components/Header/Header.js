613a1425a4d3ca0fba08516bab37d7ee9535919f
Gabriel Passos <contato.gabrielhp@gmail.com>
2020-04-12 04:13:01 -0300

    Header v.3 + Home background

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,18 +9,27 @@ export default class Header extends Component {
   render() {
     return (
       <>
-        <header className="">
-          <nav className="menus">
-            <Link className="logo-img" to="/"><img src={logo} alt="Logo Lineage 2 Battle"/></Link>
+        <header>
+          <Link className="logo-img" to="/"><img src={logo} alt="Logo Lineage 2 Battle"/></Link>
+          <nav>
             <div className="menu-principal">
               <li><Link className="" to="/">Home</Link></li>
-              <li><Link className="" to="/register">Register</Link></li>
-              <li><Link className="link-menu-login" to="/profile">Sign in</Link></li>
-            </div>
-            <div className="menu-secundario">
-              <li><Link className="button" to="/login">Sign in</Link></li>
+              <li><Link className="" to="/register">Informação</Link></li>
+              <li><Link className="link-menu-login" to="/profile">Cadastro</Link></li>
+              <li><Link className="link-menu-login" to="/profile">Contato</Link></li>
+              <li><Link className="link-menu-login" to="/profile">Doação</Link></li>
+              <li><Link className="link-menu-login" to="/profile">História</Link></li>
+              <li><Link className="link-menu-login" to="/profile">Classes</Link></li>
             </div>
           </nav>
+          <div className="menu-users">
+            <div className="button">
+              <li><Link to="/profile">Downloads</Link></li>
+            </div>
+            <div className="button">
+              <li><Link to="/login">Sign in</Link></li>
+            </div>
+          </div>
         </header>
       </>
     )
