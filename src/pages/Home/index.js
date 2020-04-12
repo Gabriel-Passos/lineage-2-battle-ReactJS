8e1eb4b35ed8dc889490a181e8a9a16dca3af959
kamaelhugo <kamaelhugo@gmail.com>
2020-04-12 03:59:20 -0300

    pacote de icones e background

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import Header from '../../components/Header/Header';
 
+import './style.css';
+
 export default class Home extends Component {
   render() {
     return (
