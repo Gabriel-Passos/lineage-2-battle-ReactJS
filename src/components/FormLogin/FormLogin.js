d6e506aed613dfe2038fc437881ad6753fef8785
Gabriel Passos <contato.gabrielhp@gmail.com>
2020-04-12 17:19:58 -0300

    Form Login v.2

--- src/components/FormLogin/FormLogin.js
+++ src/components/FormLogin/FormLogin.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './style.css';
 
@@ -6,14 +7,20 @@ export default class FormLogin extends Component {
   render() {
     return (
       <>
-        <section className="container-login">
-          <form action="">
-            <h3>Login</h3>
-            <input type="email" placeholder="E-mail"/>
-            <input type="password" placeholder="Senha"/>
-            <button type="submit">Entrar</button>
-          </form>
-        </section>
+        <div className="container-login">
+          <div className="content">
+            <section>
+              <form onSubmit="">
+                <h3>Login</h3>
+                <input type="email" placeholder="E-mail"/>
+                <input type="password" placeholder="Senha"/>
+                <div className="button-login">
+                  <button type="submit">Entrar</button>
+                </div>
+              </form>
+            </section>
+          </div>
+        </div>
       </>
     );
   }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-import Header from '../../components/Header/Header';
 import FormLogin from '../../components/FormLogin/FormLogin';
 
 export default class Login extends Component {
   render() {
     return (
       <>
-        <Header />
         <FormLogin />
       </>
     );
