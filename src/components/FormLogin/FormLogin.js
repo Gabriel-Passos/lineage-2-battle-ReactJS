9c8180d6774750e0bffada432461842b6b912e67
Gabriel Passos <contato.gabrielhp@gmail.com>
2020-04-13 00:15:37 -0300

    Login page & Register page

--- src/components/FormLogin/FormLogin.js
+++ src/components/FormLogin/FormLogin.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import {FiArrowLeft} from 'react-icons/fi';
+import {FiArrowLeft, FiArrowRight} from 'react-icons/fi';
 
 import './style.css';
 
 export default class FormLogin extends Component {
   render() {
     return (
-      <>
-        <div className="container-login">
-          <div className="content">
-            <form onSubmit="">
-              <h3>Login</h3>
-              <input type="email" placeholder="E-mail"/>
-              <input type="password" placeholder="Senha"/>
-              <button type="submit">Entrar</button>
-              <Link className="back-link" to="/">
-              <FiArrowLeft size={16} color="#ffbb00" />
-                Voltar para home
-              </Link>
-            </form>
+      <div className="container-login">
+        <div className="content-login">
+          <form onSubmit={()=>{}}>
+            <h1>Login</h1>
+            <input type="email" placeholder="E-mail"/>
+            <input type="password" placeholder="Senha"/>
+            <button type="submit">Entrar</button>
+          </form>
+          <div className="links-login">
+            <Link className="back-link-login" to="/">
+              <FiArrowLeft className="arrow-left-login" size={16} color="#ffbb00" /> Voltar para home
+            </Link>
+            <Link className="foward-link-login" to="/register">
+              Não possui uma conta? <FiArrowRight className="arrow-right-login" size={16} color="#ffbb00"/>
+            </Link>
           </div>
         </div>
-      </>
+      </div>
     );
   }
 }
