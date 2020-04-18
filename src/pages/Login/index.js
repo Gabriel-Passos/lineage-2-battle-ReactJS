03644c15dd80c368073a0cf3d72730e13246752c
Gabriel Passos <contato.gabrielhp@gmail.com>
2020-04-18 19:27:49 -0300

    login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import {FiArrowLeft, FiArrowRight} from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './style.css';
 
 export default function Login() {
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+  const history = useHistory();
+
+  async function handleLogin(e){
+    e.preventDefault();
+
+    const data =({
+      login,
+      senha
+    });
+
+    try{
+       await api.post('/login.php', data);
+
+      // if(response.data.result === true){
+        history.push('/profile');
+      
+    }catch(err){
+      alert('Falha no login, tente novamente mais tarde.');
+    }
+
+  }
+
   return (
     <div className="container-login">
       <div className="content-login">
-        <form onSubmit={()=>{}}>
+        <form onSubmit={handleLogin}>
           <h1>Login</h1>
-          <input type="email" placeholder="E-mail"/>
-          <input type="password" placeholder="Senha"/>
+          <input 
+            type="text" 
+            placeholder="E-mail"
+            value={login}
+            onChange={e =>setLogin(e.target.value)}
+          />
+          <input 
+            type="password" 
+            placeholder="Senha"
+            value={senha}
+            onChange={e => setSenha(e.target.value)}
+          />
           <button type="submit">Entrar</button>
         </form>
         <div className="links-login">
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import {FiHexagon, FiInstagram, FiFacebook, FiGithub, FiLink} from 'react-icons/fi'
 
 import Header from '../../components/Header/Header';
 
+import './style.css';
+
+import api from '../../services/api';
+
+import ImgDivMenu from '../../assets/images/imgDivMenu.png';
+import logo from '../../assets/images/l2-logo.png';
+import interludeLogo from '../../assets/images/interlude-logo.png';
+
 export default function Profile() {
+  // const [profile, setProfile] = useState([]);
+
+  const history = useHistory();
+
+  const userName = localStorage.getItem('userName');
+
+  useEffect(() => {
+    api.get('profile').then(response => {});
+  });
+
+  function handleLogout(){
+    localStorage.clear();
+    history.push('/');
+  }
+
     return (
-      <Header />
+      <>
+        <Header />
+        <div className="container-profile">
+          <div className="container-menu-left">
+            <div className="imgtop">
+              <img src={ImgDivMenu} alt=""/>
+            </div>
+            <div className="menu">
+              <li><Link to="/"><FiHexagon size={10} color="#fff"/> {userName}</Link></li>
+              <li><Link to="/"><FiHexagon size={10} color="#fff"/> Informação</Link></li>
+              <li><Link to="/register"><FiHexagon size={10} color="#fff"/> Cadastro</Link></li>
+              <li><Link to="/"><FiHexagon size={10} color="#fff"/> Doação</Link></li>
+              <li><Link to="/"><FiHexagon size={10} color="#fff"/> História</Link></li>
+              <li><Link to="/"><FiHexagon size={10} color="#fff"/> Classes</Link></li>
+            </div>
+            <div className="imgbottom">
+              <img src={ImgDivMenu} alt=""/> 
+            </div>
+          </div>
+          <div className="btn-play">
+            <img src={logo} alt=""/>
+            <div className="interlude-logo">
+              <img src={interludeLogo} alt=""/>
+            </div>
+            <Link to="/" className="myButton">JOGAR</Link>
+          </div>
+          <div className="container-menu-right">
+            <div className="imgtop">
+              <img src={ImgDivMenu} alt=""/>
+            </div>
+            <div className="menu">
+              <li><Link to="/"><FiInstagram size={20} color="#fff"/></Link></li>
+              <li><Link to="/"><FiFacebook size={20} color="#fff"/></Link></li>
+              <li><Link to="/"><FiGithub size={20} color="#fff"/></Link></li>
+              <li><Link to="/"><FiLink size={20} color="#fff"/></Link></li>
+            </div>
+            <div className="imgbottom">
+              <img src={ImgDivMenu} alt=""/>
+            </div>
+          </div>
+        </div>        
+      </>
     )
 }
