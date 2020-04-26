b73e68e3d757418c3c7c5e3f923db803477130ce
Gabriel Passos <contato.gabrielhp@gmail.com>
2020-04-26 18:55:17 -0300

    height 100%

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://cors-anywhere.herokuapp.com/http://l2infinityworld.com/function/`,
+  baseURL: `http://l2infinityworld.com/function/`,
   headers: {
     'Content-Type': 'application/json'
   }
