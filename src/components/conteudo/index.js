4b1cf0d47e150175c2de718d0300725e1bad76f9
kamaelhugo <kamaelhugo@gmail.com>
2020-05-05 00:24:21 -0300

    alteracao do header e implementacao da pagina principal

--- src/components/conteudo/index.js
+++ src/components/conteudo/index.js
@@ -6,8 +6,125 @@ export default function Conteudo(){
     return(
         <>
         <div className="content-principal">
-            <div className="separator"></div>
-            <div></div>
+            <div className="conteudo">
+                <div className="separator"></div>
+                <div className="info-pvp">
+                    <h1>INFORMAÇÕES GERAIS DOS RATES</h1>
+                    <p>
+                    RateXp = 5.<br/>
+                    RateSp = 5.<br/>
+                    RatePartyXp = 7.<br/>
+                    RatePartySp = 7.<br/>
+                    RateDropAdena = 10<br/>
+                    RateDropItems = 12<br/>
+                    RateDropSealStones = 10<br/>
+                    </p>
+                    <br/>
+                    <h1>SPOIL E FISH</h1>
+                   
+                    <p>
+                    RateDropSpoil = 18<br/>
+                    RateRaidDropItems = 20<br/>
+                    RateDropManor = 20<br/>
+                    RateExtractFish = 20<br/>
+                    </p>
+                    <br/>
+                    <h1>RATE DE QUEST</h1>
+                  
+                    <p>
+                    RateDropQuest = 15<br/>
+                    RateQuestsRewardExpSp = 8<br/>
+                    RateQuestsRewardAdena = 12<br/>
+                    RateQuestsRewardItems = 12<br/>
+                    RateCraftCost = 10<br/>
+                    RateSiegeGuardsPrice = 30<br/>
+                    </p>
+                    <br/>
+
+                    <h1>Karma (PK)</h1>
+                  
+                    <p>
+                    KarmaDropLimit = 10<br/>
+                    KarmaRateDrop = 70<br/>
+                    KarmaRateDropItem = 50<br/>
+                    KarmaRateDropEquip = 40<br/>
+                    KarmaRateDropEquipWeapon = 10<br/>
+                    RateKarmaExpLost = 20<br/>
+                    </p>
+                    <br/>
+                    <h1>RATE HEBRS</h1>
+        
+                    <p>
+                    RateCommonHerbs = 8<br/>
+                    RateHpMpHerbs = 7<br/>
+                    RateGreaterHerbs = 4<br/>
+                    RateSuperiorHerbs = 3<br/>
+                    RateSpecialHerbs = 5<br/>
+                    </p>
+                </div>
+                <div className="separator"></div>
+                    <div className="container-raca">
+                        <div className="content-card">
+                            <div className="img-raca humano img-card-size"></div>
+                            <div className="racao-texto">
+                                Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                 Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                 Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                 Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                 Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                </div>
+                        </div>
+                        <div className="content-card">
+                        <div className="img-raca elfo img-card-size"></div>
+                             <div className="racao-texto">
+                             Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                                Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                             </div>
+                        </div>
+                        <div className="content-card">
+                            <div className="img-raca darkelfo img-card-size"></div>
+                             <div className="racao-texto">
+                             Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                             </div>
+                        </div>
+                        <div className="content-card">
+                            <div className="img-raca orc img-card-size"></div>
+                             <div className="racao-texto">
+                             Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                             </div>
+                        </div>
+                        <div className="content-card">
+                            <div className="img-raca anao img-card-size"></div>
+                             <div className="racao-texto">
+                             Acendemos uma fogueira numa sala abandonada do colégio. Por estar onde estávamos e por sermos quem somos sabíamos que nunca iriam nos achar.
+                                No escuro e no frio ficamos escutando músicas e, enquanto isso, o fogo queimava o chão. Aos risos o vimos começar a consumir os avisos e os papéis afixados nas paredes daquela sala, e então ficamos presos entre as chamas e a porta. Não tínhamos como sair.
+                             </div>
+                        </div>
+                    </div>
+            </div>
         </div>
         </>
     )
