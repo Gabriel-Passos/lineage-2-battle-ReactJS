efedd600bb4aaabd1d90844a5336779bd8c6812e
Gabriel Passos <contato.gabrielhp@gmail.com>
2020-05-16 00:37:40 -0300

    profile page version 2 finished

--- src/pages/ProfileV2/index.js
+++ src/pages/ProfileV2/index.js
@@ -1,61 +1,75 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-import {ConatinerProfile, Header, PictureProfile, Menu} from './style';
+import {ConatinerProfile, Header, PictureProfile, Menu, ItensMenu} from './style';
 
 const ProfileV2 = () => {
   return (
     <>
       <ConatinerProfile>
-        <Header>
-          <header>
-            <h1>Seja bem-vindo ao seu perfil, wederdark2</h1>
-            <button>Sair</button>
-          </header>
-        </Header>
-
-        <PictureProfile>
-          {/* <img src="https://www.lineage2.com/images/desktop/classic/180919-L2-Classic-Heroes.jpg" alt=""/>
-          <Link>Alterar imagem</Link> */}
-        </PictureProfile>
-
-        <Menu>
-          <h2>Menu</h2>
-
-          <div>
-            <strong>Characters</strong>
-            <Link>Personagens</Link>
-          </div>
-
-          <div>
-            <strong>Conta</strong>
+        <section>
+          <Header>
             <div>
-              <Link>Informações da conta</Link>
-              <Link>Alterar senha</Link>
+              <h1>Seja bem-vindo ao seu perfil, wederdark2</h1>
+              <button>Sair</button>
             </div>
-          </div>
+          </Header>
 
-          <div>
-            <strong>Recursos</strong>
+          <PictureProfile>
             <div>
-              <Link>Ver inventário</Link>
-              <Link>Leilão</Link>
-              <Link>Boss live</Link>
-              <Link>Mapa live</Link>
+              <img src="https://www.lineage2.com/images/desktop/classic/180919-L2-Classic-Heroes.jpg" alt=""/>
+              <Link>Alterar imagem</Link>
             </div>
-          </div>
+          </PictureProfile>
 
-          <div>
-            <strong>Suporte</strong>
-            <Link>Enviar e-mail</Link>
-          </div>
+          <Menu>
+            <h2>Menu</h2>
 
-          <div>
-            <strong>Shop</strong>
-            <Link>Produto</Link>
-            <Link>Histórico de compra</Link>
-          </div>
-        </Menu>
+            <ItensMenu>
+              <div>
+                <strong>Characters</strong>
+                <Link>Personagens</Link>
+              </div>
+            </ItensMenu>
+
+            <ItensMenu>
+              <div>
+                <strong>Conta</strong>
+                <div>
+                  <Link>Informações da conta</Link>
+                  <Link>Alterar senha</Link>
+                </div>
+              </div>
+            </ItensMenu>
+
+            <ItensMenu>
+              <div>
+                <strong>Recursos</strong>
+                <div>
+                  <Link>Ver inventário</Link>
+                  <Link>Leilão</Link>
+                  <Link>Boss live</Link>
+                  <Link>Mapa live</Link>
+                </div>
+              </div>
+            </ItensMenu>
+
+            <ItensMenu>
+              <div>
+                <strong>Suporte</strong>
+                <Link>Enviar e-mail</Link>
+              </div>
+            </ItensMenu>
+
+            <ItensMenu>
+              <div>
+                <strong>Shop</strong>
+                <Link>Produto</Link>
+                <Link>Histórico de compra</Link>
+              </div>
+            </ItensMenu>
+          </Menu>
+        </section>
       </ConatinerProfile>
     </>
   );
--- src/pages/ProfileV2/style.js
+++ src/pages/ProfileV2/style.js
@@ -1,13 +1,28 @@
 import styled from 'styled-components';
+import {shade} from 'polished';
+
+import ImgBackground from '../../assets/images/bg-profile.jpg';
 
 export const ConatinerProfile = styled.section`
-  max-width: 860px;
-  margin: 0 auto;
-  padding: 20px;
+  /* background-image: url(${ImgBackground});
+  background-size: cover;
+  background-repeat: no-repeat; */
+
+  height: 100vh;
+
+  overflow: scroll;
+  overflow-x: hidden;
+
+  section{
+    max-width: 860px;
+    margin: 0 auto;
+    padding: 20px;
+    margin-bottom: 30px;
+  }
 `;
 
-export const Header = styled.header`
-  header{
+export const Header = styled.div`
+  div{
     display: flex;
     justify-content: space-between;
 
@@ -21,13 +36,22 @@ export const Header = styled.header`
     }
 
     button{
+      height: 40px;
+
       border: 0;
       border-radius: 5px;
-      padding: 8px 16px;
+      padding: 0 16px;
 
       font-family: Roboto, sans-serif;
       font-size: 18px;
       text-align: center;
+      color: #fff;
+
+      background-color: #0779e4;
+
+      &:hover{
+        background-color: ${shade(0.2, '#0779e4')};
+      }
     }
   }
 `;
@@ -35,9 +59,26 @@ export const Header = styled.header`
 export const PictureProfile = styled.div`
   img{
     margin-top: 50px;
-    max-width: 100px;
-    height: 100px;
+    max-width: 150px;
+    height: 150px;
     border-radius: 50%;
+  }
+
+  a{
+    display: block;
+    margin-top: 5px;
+    margin-bottom: 40px;
+    padding: 15px;
+
+    font-family: Roboto, sans-serif;
+    font-size: 18px;
+    color: #0779e4;
+
+    text-decoration: none;
+
+    &:hover{
+      color: ${shade(0.2, '#0779e4')};
+    }
   }
 `;
 
@@ -48,5 +89,42 @@ export const Menu = styled.section`
     font-family: Roboto, sans-serif;
     font-size: 30px;
     font-weight: bold;
+  }
+`;
+
+export const ItensMenu = styled.div`
+  margin-top: 30px;
+
+  strong{
+    width: 860px;
+
+    font-family: Roboto, sans-serif;
+    font-size: 30px;
+    font-weight: 100;
+  }
+
+  a{
+    margin-top: 13px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    max-width: 860px;
+    padding: 12px;
+
+    background-color: #0779e4;
+
+    color: #fff;
+    font-family: Roboto, sans-serif;
+    font-size:18px;
+
+    border-radius: 5px;
+
+    text-decoration: none;
+
+    &:hover{
+      background-color: ${shade(0.2, '#0779e4')};
+    }
   }
 `;
